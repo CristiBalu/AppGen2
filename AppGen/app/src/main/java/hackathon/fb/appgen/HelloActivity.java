27a8acf3b0adef7a8d0e2b8f1f2e29ad58152976
Cristi <cristian.balu@outlook.com>
2017-03-11 12:07:49 +0000

    Created the project

---
+++ AppGen/app/src/main/java/hackathon/fb/appgen/HelloActivity.java
@@ -0,0 +1,13 @@
+package hackathon.fb.appgen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HelloActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.hello_layout);
+    }
+}
