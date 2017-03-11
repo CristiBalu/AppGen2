84cc8cac941edb1df78e35a63565a02269a2d5ef
Cristi <cristian.balu@outlook.com>
2017-03-11 13:51:51 +0000

    Speech: implemented speech recognition in a sample activity

---
+++ AppGen/app/src/main/java/fbhack/martaungureanu/appgen/SpeechActivity.java
@@ -0,0 +1,62 @@
+package fbhack.martaungureanu.appgen;
+
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.speech.RecognizerIntent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+public class SpeechActivity extends AppCompatActivity {
+    Button listenButton;
+    private final int REQ_CODE_SPEECH_INPUT = 100;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_speech);
+
+        listenButton = (Button) findViewById(R.id.listen_button);
+        listenButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                promptSpeechInput();
+            }
+        });
+    }
+
+    private void promptSpeechInput() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+
+        try {
+            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+        } catch (ActivityNotFoundException a) {
+            Toast.makeText(getApplicationContext(),
+                    getText(R.string.speech_not_supported),
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch(requestCode) {
+            case REQ_CODE_SPEECH_INPUT:
+                if(resultCode == RESULT_OK && null != data) {
+                    ArrayList<String> result = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    Toast.makeText(getApplicationContext(), result.get(0), Toast.LENGTH_SHORT);
+                }
+                break;
+        }
+    }
+}
---
+++ AppGen/app/src/main/java/fbhack/martaungureanu/appgen/utils/Cognitive.java
@@ -0,0 +1,9 @@
+package fbhack.martaungureanu.appgen.utils;
+
+/**
+ * Created by crist on 11/03/2017.
+ */
+
+public class Cognitive {
+
+}
