358431d40d1122165dadc60816927e760f6e5b2a
mu515 <mu515@ic.ac.uk>
2017-03-11 17:05:17 +0000

    Customized SpeechActivity

--- AppGen/app/src/main/java/fbhack/martaungureanu/appgen/HelloActivity.java
+++ AppGen/app/src/main/java/fbhack/martaungureanu/appgen/HelloActivity.java
@@ -1,13 +1,30 @@
 package fbhack.martaungureanu.appgen;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HelloActivity extends AppCompatActivity {
+    Button getStartedButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.hello_layout);
+
+        getStartedButton = (Button) findViewById(R.id.get_started_button);
+        getStartedButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                startSpeechActivity();
+            }
+        });
+    }
+
+    public void startSpeechActivity() {
+        Intent intent = new Intent(this, SpeechActivity.class);
+        startActivity(intent);
     }
 }
